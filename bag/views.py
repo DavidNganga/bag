41076b9e3d11ce7d0a7698329f945b2ce8a345d1
DavidNganga <ngashiedavid@gmail.com>
2019-03-31 18:25:44 +0300

    installed bootstrap

--- bag/views.py
+++ bag/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def welcome(request):
 
     return render(request, 'welcome.html')
+
+def post(request):
+    form= PostForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.save()
+        return redirect('welcome')
+    else:
+        form = PostForm()
+    return render(request, 'post.html', {"form":form})
