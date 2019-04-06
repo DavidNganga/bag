3e9707ba3ae55ae4958562b30f35548a0c63d833
DavidNganga <ngashiedavid@gmail.com>
2019-04-06 10:20:35 +0300

    search works

--- bread/settings.py
+++ bread/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'bread.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'david',
+        'PASSWORD': 'somanypasswords',
     }
 }
 
--- bread/urls.py
+++ bread/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
-
+from django.contrib.auth import views
 
 urlpatterns = [
-    path('bag/', include('bag.urls')),
     path('admin/', admin.site.urls),
+    path('bag/', include('bag.urls')),
+
 ]
